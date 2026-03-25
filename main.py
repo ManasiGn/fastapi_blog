5eedb35643481f8efd1dcc152956f218468b503f
Manasi G <manasign0@gmail.com>
2026-03-25 13:46:14 +0530

    added html,css and required images

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 posts: list[dict] = [
     {
         "id": 1,
-        "author": "Corey Schafer",
+        "author": "Manasi",
         "title": "FastAPI is Awesome",
         "content": "This framework is really easy to use and super fast.",
         "date_posted": "April 20, 2025",
     },
     {
         "id": 2,
-        "author": "Jane Doe",
+        "author": "Rithu Raj",
         "title": "Python is Great for Web Development",
         "content": "Python is a great language for web development, and FastAPI makes it even better.",
         "date_posted": "April 21, 2025",
-    }
+    },
 ]
 
-#include_in_Schema is used to add the api in documetation
-@app.get("/", include_in_schema=False) 
-@app.get("/posts", include_in_schema=False)
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
     return templates.TemplateResponse(
-        request, 
-        "home.html", 
+        request,
+        "home.html",
         {"posts": posts, "title": "Home"},
     )
+
 
 @app.get("/api/posts")
 def get_posts():
